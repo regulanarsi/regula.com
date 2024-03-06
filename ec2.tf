872d4a6075ea5da5b6d9a5301b43fdd1c09e1b23
regulanarsi <34062263+regulanarsi@users.noreply.github.com>
2024-03-06 19:44:38 +0530

    Create ec2.tf
    test

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-0323c3dd2da7fb37d"
+  instance_type = var.instance_type
+}
+
+locals {
+   instance_type = "m2.medium" 
+}
